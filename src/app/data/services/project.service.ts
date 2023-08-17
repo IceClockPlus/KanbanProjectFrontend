e163c15589fe30e6a0a52ce2ecb7668f940a0f3f
IceClockPlus <aravenapro98@gmail.com>
2023-08-16 23:19:33 -0400

    Creado mock service de llamada a API

--- src/app/data/services/project.service.ts
+++ src/app/data/services/project.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IProject } from '@data/intefaces/project';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,26 @@ import { Injectable } from '@angular/core';
 export class ProjectService {
 
   constructor() { }
+
+  private mockProjects: IProject[] = [
+    {
+      id: 'PD-1',
+      name: 'Project  1',
+      description: 'Description 1'
+    },
+    {
+      id: 'PD-2',
+      name: 'Project  2',
+      description: 'Description 2'
+    },
+    {
+      id: 'PD-3',
+      name: 'Project  3',
+      description: 'Description 3'
+    },
+  ];
+
+  public getMyProjects(): Observable<IProject[]> {
+    return of(this.mockProjects);
+  }
 }
